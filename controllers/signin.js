36e7b63c173cd51ee2b3ecda8066e9f49d16324b
F1r3byte <f1r3byte@yandex.com>
2019-01-17 10:57:58 -1200

    added validation

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,25 +1,29 @@
-const handleSignin = (database, bcrypt) =>  (req, res) => {
-	database
-		.select("email", "hash")
-		.from("login")
-		.where("email", "=", req.body.email)
-		.then(data => {
-			const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-			if (isValid) {
-				return database
-					.select("*")
-					.from("users")
-					.where("email", "=", req.body.email)
-					.then(data => res.json(data[0]))
-					.catch(err => res.status(400).json("Wrong email or password."));
-			} else {
-				res.status(400).json("Wrong email or password.");
-			}
-		})
-		.catch(err => res.status(400).json("Wrong email or password."));
+const handleSignin = (database, bcrypt) => (req, res) => {
+	if (name || email || password) {
+		database
+			.select("email", "hash")
+			.from("login")
+			.where("email", "=", req.body.email)
+			.then(data => {
+				const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+				if (isValid) {
+					return database
+						.select("*")
+						.from("users")
+						.where("email", "=", req.body.email)
+						.then(data => res.json(data[0]))
+						.catch(err => res.status(400).json("Wrong email or password."));
+				} else {
+					res.status(400).json("Wrong email or password.");
+				}
+			})
+			.catch(err => res.status(400).json("Wrong email or password."));
+	} else {
+		res.status(400).json("Something went wrong! Please try again.")
+	}
 }
 
 
-		module.exports = {
-			handleSignin: handleSignin
-		}
+module.exports = {
+	handleSignin: handleSignin
+}
