45df20804f2414cdf7c1b7f1bb5d12b89b376c54
F1r3byte <f1r3byte@yandex.com>
2019-01-17 11:02:35 -1200

    using destructuring

--- controllers/signin.js
+++ controllers/signin.js
@@ -7,14 +7,14 @@ const handleSignin = (database, bcrypt) => (req, res) => {
 		database
 			.select("email", "hash")
 			.from("login")
-			.where("email", "=", req.body.email)
+			.where("email", "=", email)
 			.then(data => {
-				const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+				const isValid = bcrypt.compareSync(password, data[0].hash);
 				if (isValid) {
 					return database
 						.select("*")
 						.from("users")
-						.where("email", "=", req.body.email)
+						.where("email", "=", email)
 						.then(data => res.json(data[0]))
 						.catch(err => res.status(400).json("Wrong email or password."));
 				} else {
