797877b895c5ef68d1cbc81f85ff549836bb6a66
F1r3byte <f1r3byte@yandex.com>
2019-01-17 23:50:20 +0100

    added validation

--- controllers/signup.js
+++ controllers/signup.js
@@ -1,35 +1,43 @@
 const handleSignup = (database, bcrypt) => (req, res) => {
-	const { name, email, password } = req.body;
-	const hash = bcrypt.hashSync(password);
-	database
-		.transaction(trx => {
-			trx
-				.insert({
-					hash: hash,
-					email: email
-				})
-				.into("login")
-				.returning("email")
-				.then(signupEmail => {
-					return trx("users")
-						.returning("*")
-						.insert({
-							email: signupEmail[0],
-							name: name,
-							joined: new Date()
-						})
-						.then(user => {
-							res.json(user[0]);
-						});
-				})
-				.then(trx.commit)
-				.catch(trx.rollback);
-		})
-		.catch(err =>
-			res.status(400).json("Something went wrong! Please try again.")
-		);
+	const {
+		name,
+		email,
+		password
+	} = req.body;
+	if (name || email || password) {
+		const hash = bcrypt.hashSync(password);
+		database
+			.transaction(trx => {
+				trx
+					.insert({
+						hash: hash,
+						email: email
+					})
+					.into("login")
+					.returning("email")
+					.then(signupEmail => {
+						return trx("users")
+							.returning("*")
+							.insert({
+								email: signupEmail[0],
+								name: name,
+								joined: new Date()
+							})
+							.then(user => {
+								res.json(user[0]);
+							});
+					})
+					.then(trx.commit)
+					.catch(trx.rollback);
+			})
+			.catch(err =>
+				res.status(400).json("Something went wrong! Please try again.")
+			);
+	} else {
+		res.status(400).json("Something went wrong! Please try again.")
+	}
 }
 
-module.exports ={
+module.exports = {
 	handleSignup: handleSignup
 }
